4b7f02dc45c91b9424ed4fc0d723de809f29d9a6
Marco Lemos <marco.trab4@gmail.com>
2023-02-11 00:03:32 -0300

    test:  add test for post and get articles
    - add conftest fixtures
    - add mongo img in continuous_integration

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -1,2 +1,23 @@
-def test_teste():
-    assert True
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_post_article(client: AsyncClient):
+    response = await client.post(
+        '/articles',
+        json={'title': 'Foo', 'content': 'foo bar'},
+    )
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    if response.json():
+        assert isinstance(response.json()['title'], str)
+
+
+@pytest.mark.anyio
+async def test_get_article(client: AsyncClient):
+    response = await client.get('/articles')
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    if response.json():
+        assert isinstance(response.json()[0]['title'], str)
